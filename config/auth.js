67955e26e36829b15a9b6e5720db2456f4e0541b
Oduntan Oluwatobiloba Stephen <stephen.oduntan@andela.com>
2016-05-24 23:53:37 +0100

    first commit

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  'facebookAuth': {
+    'clientID': '792948227472428', // your App ID
+    'clientSecret': '974d35b6ec5d71bedae574cf8615c240', // your App Secret
+    'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+  },
+
+  'twitterAuth': {
+    'consumerKey': 'your-consumer-key-here',
+    'consumerSecret': 'your-client-secret-here',
+    'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+  },
+
+  'googleAuth': {
+    'clientID': 'your-secret-clientID-here',
+    'clientSecret': 'your-client-secret-here',
+    'callbackURL': 'http://localhost:8080/auth/google/callback'
+  }
+
+};
