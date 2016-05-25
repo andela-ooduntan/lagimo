fa14740c918f72e247c812b266b719eb9c6159b1
Oduntan Oluwatobiloba Stephen <stephen.oduntan@andela.com>
2016-05-25 02:02:05 +0100

    Updated mongoose file

--- config/auth.js
+++ config/auth.js
@@ -1,21 +1,21 @@
 module.exports = {
 
   'facebookAuth': {
-    'clientID': '792948227472428', // your App ID
-    'clientSecret': '974d35b6ec5d71bedae574cf8615c240', // your App Secret
-    'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+    'clientID': '792948227472428',
+    'clientSecret': '974d35b6ec5d71bedae574cf8615c240',
+    'callbackURL': 'http://lagimo.herokuapp.com/auth/facebook/callback'
   },
 
   'twitterAuth': {
     'consumerKey': 'your-consumer-key-here',
     'consumerSecret': 'your-client-secret-here',
-    'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+    'callbackURL': 'http://lagimo.herokuapp.com/auth/twitter/callback'
   },
 
   'googleAuth': {
     'clientID': 'your-secret-clientID-here',
     'clientSecret': 'your-client-secret-here',
-    'callbackURL': 'http://localhost:8080/auth/google/callback'
+    'callbackURL': 'http://lagimo.herokuapp.com/auth/google/callback'
   }
 
 };
