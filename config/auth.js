ed1b91a0e89fb317ca90d545330a3f540ace74ff
Oduntan Oluwatobiloba Stephen <stephen.oduntan@andela.com>
2016-05-25 08:59:02 +0100

    Modified auth and database file

--- config/auth.js
+++ config/auth.js
@@ -3,19 +3,19 @@ module.exports = {
   'facebookAuth': {
     'clientID': '792948227472428',
     'clientSecret': '974d35b6ec5d71bedae574cf8615c240',
-    'callbackURL': 'http://lagimo.herokuapp.com/auth/facebook/callback'
+    'callbackURL': 'http://localhost:8080/auth/facebook/callback'
   },
 
   'twitterAuth': {
     'consumerKey': 'your-consumer-key-here',
     'consumerSecret': 'your-client-secret-here',
-    'callbackURL': 'http://lagimo.herokuapp.com/auth/twitter/callback'
+    'callbackURL': 'http://localhost:8080/auth/twitter/callback'
   },
 
   'googleAuth': {
     'clientID': 'your-secret-clientID-here',
     'clientSecret': 'your-client-secret-here',
-    'callbackURL': 'http://lagimo.herokuapp.com/auth/google/callback'
+    'callbackURL': 'http://localhost:8080/auth/google/callback'
   }
 
 };
--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'mongodb://tobiloba:odunnntan@ds057254.mlab.com:57254/heroku_ppn2qdx1',
+  url: 'mongodb://127.0.0.1:27017/facebookAuth',
 };
