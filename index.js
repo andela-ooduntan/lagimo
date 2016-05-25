c1391dd7c3fa3c020fcbaa8fcdb637e75ab8e514
Oduntan Oluwatobiloba Stephen <stephen.oduntan@andela.com>
2016-05-25 02:21:56 +0100

    made correction

--- index.js
+++ index.js
@@ -12,27 +12,23 @@ var session = require('express-session');
 
 var configDB = require('./config/database.js');
 
-// configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+mongoose.connect(configDB.url);
 
-require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport);
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
 
-// required for passport
-app.use(session({ secret: 'teamvvida' })); // session secret
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'teamvvida' }));
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(passport.session());
+app.use(flash());
 
-// routes ======================================================================
-require('./app/routes')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes')(app, passport);
 
-// launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
